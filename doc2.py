5eae0fd4aa57d1913c58159b9832ecc4c9421e6e
Vinay Kumar Dongari <vinaykumardongari@Vinays-MacBook-Pro.local>
2019-09-18 22:56:37 -0700

    adding files

--- doc2.py
+++ doc2.py
@@ -1,1 +1,14 @@
-df dd
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
+
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Why Hello %s!\n' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000) 
